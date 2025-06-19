405c4b0df688732e682884e5de2bb05f038f9483
makenz <mackenzchung@gmail.com>
2025-06-18 19:57:58 -0500

    trying out stuff

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ document.querySelectorAll('.nav-link').forEach(link => {
     });
 });
 
-
 // Animate wave effect on letters
 document.querySelectorAll('.wave-animate').forEach(element => {
     const text = element.textContent;
@@ -23,3 +22,39 @@ document.querySelectorAll('.wave-animate').forEach(element => {
     }
     });
 });
+
+
+
+// Eyes follow mouse
+document.querySelector('body').addEventListener('mousemove', eyeballFollowMouse);
+
+function eyeballFollowMouse() {
+    let eyes = document.querySelectorAll('.eye');
+    eyes.forEach(eye => {
+        let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+        let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let angle = radian * (180 / Math.PI) * -1 + 270;
+        eye.style.transform = `rotate(${angle}deg)`;
+    });
+}
+
+// Eye click detection
+document.querySelectorAll('.eye').forEach(eye => {
+    eye.addEventListener('click', function() {
+        console.log('Eye clicked!');
+        this.classList.add('blink');
+        
+        setTimeout(() => {
+            this.classList.remove('blink');
+        }, 200);
+    });
+});
+
+
+
+function zoomImage(image) {
+    console.log('Image clicked!');
+    image.classList.toggle('zoomed');
+}
+
